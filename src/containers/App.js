131c2be18f019ef82be438c55c3787bc4e8ec813
dubena <andrej.duben@accenture.com>
2022-07-05 12:51:33 +0200

    Changed state to hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,47 +1,37 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll'
 import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css';
 
-class App extends Component {
-    constructor(){
-        super()
-        this.state = {
-            robots: [],
-            searchflied: ''
-        }
-    }
+export default function App() {
+    const [robots, setRobots] = useState([]);
+    const [searchflied, setSearchflied] = useState('');
     
-    componentDidMount(){
+    useEffect(()=> {
         fetch('https://jsonplaceholder.cypress.io/users')
             .then(response => response.json())
-            .then(users => this.setState({robots: users}));
+            .then(users => this.setRobots(users));
+    }, [])
+
+    const onSearchChanged = (event) => {
+        setSearchflied(event.target.value)
     }
 
-    onSearchChanged = (event) => {
-        this.setState({ searchflied: event.target.value })
-    }
-
-    render() {
-        const {robots, searchflied } = this.state;
-        const filterRobots = robots.filter( robot => {
-            return robot.name.toLowerCase().includes(searchflied.toLowerCase())
-        })
+    const filterRobots = robots.filter( robot => {
+        return robot.name.toLowerCase().includes(searchflied.toLowerCase())
+    });
         
-        return (
-            <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChanged={this.onSearchChanged}/>
-                <Scroll>
-                    <ErrorBoundary>
-                        <CardList robots={filterRobots} />
-                    </ErrorBoundary>
-                </Scroll>
-            </div>
-        );
-    }
+    return (
+        <div className='tc'>
+            <h1 className='f1'>RoboFriends</h1>
+            <SearchBox searchChanged={onSearchChanged}/>
+            <Scroll>
+                <ErrorBoundary>
+                    <CardList robots={filterRobots} />
+                </ErrorBoundary>
+            </Scroll>
+        </div>
+    );
 }
-
-export default App;
